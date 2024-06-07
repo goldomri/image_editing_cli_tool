cd6aba03bc9bc64dd45000a050068b9c3fe484ba
goldomri <goldomri@gmail.com>
2024-06-07 16:36:40 +0300

    BoxBlurFilter

--- image_filters.py
+++ image_filters.py
@@ -6,6 +6,7 @@ class Filter(ABC):
     """
     Abstract class for image filters
     """
+
     CONVOLUTION_KERNEL_SIZE_ERR_MSG = "Convolution kernel size should be smaller than image size."
 
     def __init__(self, image_array: np.ndarray):
@@ -15,11 +16,12 @@ class Filter(ABC):
         """
         self._original_image_array = image_array
 
-    def _convolution(self, kernel: np.ndarray) -> np.ndarray:
+    def convolution(self, kernel: np.ndarray) -> np.ndarray:
         """
         Applies a convolution with specified kernel to the image array.
-        :param kernel: Kernel of the convolution
-        :return: The new image array after convolution operation
+        :param kernel: Kernel of the convolution.
+        :return: The new image array after convolution operation.
+        :raise: ValueError in case the kernel size is bigger than the image size.
         """
         kernel_height, kernel_width = kernel.shape
         # Checking if kernel size is valid
@@ -38,3 +40,34 @@ class Filter(ABC):
                 for k in range(new_image_array.shape[2]):
                     new_image_array[i, j, k] = np.sum(kernel * padded_image[i:i + kernel_height, j:j + kernel_width, k])
         return new_image_array
+
+    @abstractmethod
+    def apply_filter(self, x=None, y=None) -> np.ndarray:
+        """
+        Abstract method for applying the filter.
+        :param x: Optional parameter for the filter.
+        :param y: Optional parameter for the filter.
+        :return: New filtered image as a numpy array.
+        """
+        pass
+
+
+class BoxBlurFilter(Filter):
+    """
+    Class for applying the Box Blur filter.
+    """
+
+    UNSPECIFIED_KERNEL_SIZE_ERR_MSG = "Kernel size should be inputted by specifying x and y arguments."
+
+    def apply_filter(self, x=None, y=None) -> np.ndarray:
+        """
+        Method for applying the Box Blur filter.
+        :param x: x size of kernel.
+        :param y: y size of kernel.
+        :return: New filtered image as a numpy array.
+        """
+        # Checking if x and y are inputted
+        if not x or not y:
+            raise ValueError(BoxBlurFilter.UNSPECIFIED_KERNEL_SIZE_ERR_MSG)
+        kernel = np.ones((x, y)) / (x * y)
+        return self.convolution(kernel)
