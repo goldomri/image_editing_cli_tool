6ee5cc5e4a28d2641392fffa31c91953b1ac6a2f
goldomri <goldomri@gmail.com>
2024-06-07 18:12:27 +0300

    created constants file

---
+++ constants.py
@@ -0,0 +1,18 @@
+
+
+MIN_INTENSITY = 0
+MAX_INTENSITY = 255
+MIN_BRIGHTNESS_VAL = -255
+MAX_BRIGHTNESS_VAL = 255
+MIN_CONTRAST_VAL = -255
+MAX_CONTRAST_VAL = 255
+CONTRAST_NORM_CONST = 259
+CONTRAST_MID_VAL = 128
+MIN_SATURATION_VAL = -100
+MAX_SATURATION_VAL = 100
+
+INVALID_BRIGHTNESS_VAL_ERR_MSG = "Brightness adjustment value should be between -255 to 255."
+INVALID_CONTRAST_VAL_ERR_MSG = "Contrast adjustment value should be between -255 to 255."
+INVALID_SATURATION_VAL_ERR_MSG = "Saturation adjustment value should be between -100 to 100."
+CONVOLUTION_KERNEL_SIZE_ERR_MSG = "Convolution kernel size should be smaller than image size."
+UNSPECIFIED_KERNEL_SIZE_ERR_MSG = "Kernel size should be inputted by specifying x and y arguments."
